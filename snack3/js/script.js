e1c59cf8d3ece03a6adb9201b3cfa12e95fcb4ff
domenicog03 <domenico.galeone.info@gmail.com>
2024-03-11 16:17:28 +0100

    feat: fine snack 3

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -3,3 +3,14 @@
 //Chiedi per 6 volte all’utente di inserire un numero,
 //se è dispari inseriscilo nell’array.
 
+const numeri= [];
+
+for(let i=0; i<6; i++){
+    let n= parseInt(prompt('Inserisci un numero:'));
+
+    if(!(n % 2 === 0)){
+        numeri.push(n);
+    }
+}
+
+console.log(numeri);
